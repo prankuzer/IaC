72b189791922c2e895938dfd91aa039c49f01239
prankuzer <prankuzer@gmail.com>
2023-01-12 12:00:00 +0500

    Final 01-terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,14 @@ provider "aws" {
   region  = var.region
   profile = var.profile
 }
+
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-06878d265978313ca"
+  key_name      = "sergei-northv"
+  instance_type = "t2.micro"
+  tags = {
+    Name    = "My-UbuntuLinux-Server"
+    Owner   = "Sergei Epammer"
+    project = "Kingsman"
+  }
+}
