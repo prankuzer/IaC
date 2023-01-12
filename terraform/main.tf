b7904fcbebdfabe7f774ba9bcb288373a64186d9
prankuzer <prankuzer@gmail.com>
2023-01-12 22:58:46 +0500

    Final 03-Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,33 @@ provider "aws" {
   profile = var.profile
 }
 
+resource "aws_default_vpc" "default" {}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    description = "Allow port HTTPS"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow ALL ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-06878d265978313ca"
   key_name      = "sergei-northv"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_default_security_group.default.id]
   tags = {
     Name    = "My-UbuntuLinux-Server"
     Owner   = "Sergei Epammer"
